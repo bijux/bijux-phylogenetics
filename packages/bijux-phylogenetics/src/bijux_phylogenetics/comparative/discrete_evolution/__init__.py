e707dd6c27c572d1044880b5c477e725afcd4361
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-16 15:56:21 +0200

    refactor(comparative): package discrete transition reporting

---
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/comparative/discrete_evolution/__init__.py
@@ -0,0 +1,22 @@
+"""Discrete-state evolution workflows."""
+
+from .core import *  # noqa: F403
+from .core import (
+    _estimate_node_states as _estimate_node_states,
+    _estimate_transition_support_rows as _estimate_transition_support_rows,
+    _fit_transition_matrix as _fit_transition_matrix,
+    _fitch_candidate_sets as _fitch_candidate_sets,
+    _resolve_state_order as _resolve_state_order,
+    _root_prior as _root_prior,
+    _stationary_frequencies as _stationary_frequencies,
+    _transition_events as _transition_events,
+)
+from .reporting import (
+    build_biogeographic_interpretation_report as build_biogeographic_interpretation_report,
+    render_discrete_state_evolution_report as render_discrete_state_evolution_report,
+    render_tree_with_geographic_states as render_tree_with_geographic_states,
+    validate_discrete_transition_reference_examples as validate_discrete_transition_reference_examples,
+    write_discrete_model_comparison_table as write_discrete_model_comparison_table,
+    write_node_state_probability_table as write_node_state_probability_table,
+    write_transition_summary_table as write_transition_summary_table,
+)
