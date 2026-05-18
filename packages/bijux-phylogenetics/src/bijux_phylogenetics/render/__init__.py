c7cc731c8c054e9b44ee4fc50d42bdde695d7a8b
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-18 06:08:10 +0200

    feat(render): add time tree uncertainty svg

--- packages/bijux-phylogenetics/src/bijux_phylogenetics/render/__init__.py
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/render/__init__.py
@@ -2,6 +2,11 @@
 
 from .package import TreeFigurePackageResult, build_tree_figure_package
 from .svg import AnnotationStrip, TreeRenderResult, render_tree_svg
+from .time_tree_svg import (
+    TimeTreeNodeInterval,
+    TimeTreeRenderResult,
+    render_time_tree_svg,
+)
 from .trait_tree_package import (
     AnnotatedTraitTreePackageResult,
     build_annotated_trait_tree_package,
@@ -10,9 +15,12 @@ from .trait_tree_package import (
 __all__ = [
     "AnnotationStrip",
     "AnnotatedTraitTreePackageResult",
+    "TimeTreeNodeInterval",
+    "TimeTreeRenderResult",
     "TreeFigurePackageResult",
     "TreeRenderResult",
     "build_annotated_trait_tree_package",
     "build_tree_figure_package",
+    "render_time_tree_svg",
     "render_tree_svg",
 ]
