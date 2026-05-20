a0cee0a892e962eb577117811e1022335e211064
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-21 00:01:38 +0200

    refactor(cli): establish supplementary table report package

---
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/command_line/report/supplementary_tables/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from .registration import (
+    add_supplementary_table_report_commands,
+    run_supplementary_table_report_command,
+)
+
+__all__ = [
+    "add_supplementary_table_report_commands",
+    "run_supplementary_table_report_command",
+]
