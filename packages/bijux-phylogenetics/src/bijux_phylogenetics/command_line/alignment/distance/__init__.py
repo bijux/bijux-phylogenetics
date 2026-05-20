feba3038e99b4d7705d563462eea740adc6c55fd
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-20 23:21:37 +0200

    refactor(cli): establish alignment distance command package

---
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/command_line/alignment/distance/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .registration import (
+    add_alignment_distance_commands,
+    run_alignment_distance_command,
+)
+
+
+__all__ = [
+    "add_alignment_distance_commands",
+    "run_alignment_distance_command",
+]
