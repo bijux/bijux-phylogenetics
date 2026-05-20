841ea1e44e8388e49ece468969a5dc61776e04af
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-20 23:43:20 +0200

    refactor(cli): establish beast adapter command package

---
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/command_line/adapters/beast/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from .registration import add_beast_adapter_commands, run_beast_adapter_command
+
+
+__all__ = [
+    "add_beast_adapter_commands",
+    "run_beast_adapter_command",
+]
