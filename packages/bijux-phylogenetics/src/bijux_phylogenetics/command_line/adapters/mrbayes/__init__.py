597037e16d230ad1858bed87f932c177eb59dc4a
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-20 23:54:17 +0200

    refactor(cli): establish mrbayes adapter command package

---
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/command_line/adapters/mrbayes/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from .registration import (
+    add_mrbayes_adapter_commands,
+    run_mrbayes_adapter_command,
+)
+
+__all__ = [
+    "add_mrbayes_adapter_commands",
+    "run_mrbayes_adapter_command",
+]
