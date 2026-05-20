d3fb93743c0732786e460da0766cf12bbde6c831
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-20 02:09:15 +0200

    refactor(comparative): publish discrete mk package api

--- packages/bijux-phylogenetics/src/bijux_phylogenetics/comparative/discrete_mk/__init__.py
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/comparative/discrete_mk/__init__.py
@@ -1,9 +1,10 @@
+"""Discrete Mk comparative workflows."""
+
 from .comparison import (
     compare_discrete_mk_model_ranking as compare_discrete_mk_model_ranking,
     compare_discrete_mk_model_ranking_from_dataset as compare_discrete_mk_model_ranking_from_dataset,
 )
 from .fitting import (
-    build_discrete_mk_input_audit as build_discrete_mk_input_audit,
     fit_discrete_mk_model as fit_discrete_mk_model,
     fit_discrete_mk_model_from_dataset as fit_discrete_mk_model_from_dataset,
 )
@@ -26,3 +27,25 @@ from .tables import (
     write_discrete_mk_rate_table as write_discrete_mk_rate_table,
     write_discrete_mk_summary_table as write_discrete_mk_summary_table,
 )
+
+__all__ = [
+    "DISCRETE_MK_LIKELIHOOD_COMPARISON_POLICY",
+    "DISCRETE_MK_LIKELIHOOD_CONSTANT_POLICY",
+    "DISCRETE_MK_MODEL_COMPARISON_ORDER",
+    "DISCRETE_MK_MODEL_CONFIDENCE_DELTA_THRESHOLD",
+    "DISCRETE_MK_MODEL_CONFIDENCE_WEIGHT_BASIS",
+    "DISCRETE_MK_MODEL_RANKING_POLICY",
+    "DiscreteMkFitReport",
+    "DiscreteMkInputAudit",
+    "DiscreteMkModelComparisonReport",
+    "DiscreteMkTransformBaselineComparison",
+    "DiscreteMkTransformFit",
+    "DiscreteMkTransformProfileRow",
+    "DiscreteMkTransformWarning",
+    "compare_discrete_mk_model_ranking",
+    "compare_discrete_mk_model_ranking_from_dataset",
+    "fit_discrete_mk_model",
+    "fit_discrete_mk_model_from_dataset",
+    "write_discrete_mk_rate_table",
+    "write_discrete_mk_summary_table",
+]
