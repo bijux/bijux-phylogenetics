639786693b06f564f0a06bac09cb2c15129529fe
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-21 17:38:46 +0200

    refactor(benchmark): move large tree fitting runner into module

--- packages/bijux-phylogenetics/src/bijux_phylogenetics/benchmark/model_fitting/large_tree_model_fitting/__init__.py
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/benchmark/model_fitting/large_tree_model_fitting/__init__.py
@@ -12,6 +12,7 @@ from .artifact_outputs import (
     write_large_tree_model_fitting_summary_table,
 )
 from .measurement import measure_continuous_fit
+from .observation_runner import evaluate_threshold
 
 __all__ = [
     "LargeTreeModelFittingBenchmarkBundle",
@@ -19,6 +20,7 @@ __all__ = [
     "LargeTreeModelFittingObservation",
     "LargeTreeModelFittingThreshold",
     "case_definitions_for_tier",
+    "evaluate_threshold",
     "measure_continuous_fit",
     "write_large_tree_model_fitting_observation_table",
     "write_large_tree_model_fitting_summary_table",
