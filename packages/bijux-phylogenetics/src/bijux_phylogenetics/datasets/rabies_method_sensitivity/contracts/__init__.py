0444b04d5fc36f853eb4ac069f33d45c04b6a817
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-21 04:25:11 +0200

    refactor(datasets): move rabies method sensitivity workflow contracts into package

--- packages/bijux-phylogenetics/src/bijux_phylogenetics/datasets/rabies_method_sensitivity/contracts/__init__.py
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/datasets/rabies_method_sensitivity/contracts/__init__.py
@@ -5,9 +5,23 @@ from .dataset import (
     RabiesMethodSensitivityPanelExportResult,
     RabiesMethodSensitivityVariant,
 )
+from .workflow import (
+    RabiesMethodSensitivityCladeRow,
+    RabiesMethodSensitivityConclusionRow,
+    RabiesMethodSensitivityPanelWorkflowReport,
+    RabiesMethodSensitivityPreprocessingComparisonRow,
+    RabiesMethodSensitivityTaskRecord,
+    RabiesMethodSensitivityVariantRun,
+)
 
 __all__ = [
+    "RabiesMethodSensitivityCladeRow",
+    "RabiesMethodSensitivityConclusionRow",
     "RabiesMethodSensitivityPanelDataset",
     "RabiesMethodSensitivityPanelExportResult",
+    "RabiesMethodSensitivityPanelWorkflowReport",
+    "RabiesMethodSensitivityPreprocessingComparisonRow",
+    "RabiesMethodSensitivityTaskRecord",
     "RabiesMethodSensitivityVariant",
+    "RabiesMethodSensitivityVariantRun",
 ]
