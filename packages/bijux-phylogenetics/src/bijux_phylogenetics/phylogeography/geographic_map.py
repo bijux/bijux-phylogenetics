e4ae747fa9cc01a8ae7a91485f974fb0a4c96bd2
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-21 17:59:31 +0200

    refactor(phylogeography): replace geographic map import path

--- packages/bijux-phylogenetics/src/bijux_phylogenetics/phylogeography/geographic_map.py
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/phylogeography/geographic_map.py
@@ -1,39 +1,33 @@
 from __future__ import annotations
 
-from bijux_phylogenetics.phylogeography.geographic_map_review.artifact_outputs import (
-    write_geographic_map_exclusion_table as _write_geographic_map_exclusion_table,
-    write_geographic_map_line_table as _write_geographic_map_line_table,
-    write_geographic_map_marker_table as _write_geographic_map_marker_table,
-    write_geographic_map_summary_table as _write_geographic_map_summary_table,
-)
-from bijux_phylogenetics.phylogeography.geographic_map_review.continuous_review import (
-    summarize_continuous_phylogeography_map as _summarize_continuous_phylogeography_map,
-)
-from bijux_phylogenetics.phylogeography.geographic_map_review.discrete_review import (
-    summarize_discrete_region_map as _summarize_discrete_region_map,
-)
-from bijux_phylogenetics.phylogeography.geographic_map_review.presentation import (
-    render_geographic_map_html as _render_geographic_map_html,
-)
-from bijux_phylogenetics.phylogeography.geographic_map_review.contracts import (
-    GeographicMapArtifact as _GeographicMapArtifact,
-    GeographicMapExclusionRow as _GeographicMapExclusionRow,
-    GeographicMapLineRow as _GeographicMapLineRow,
-    GeographicMapMarkerRow as _GeographicMapMarkerRow,
-    GeographicMapReport as _GeographicMapReport,
-    GeographicMapSummary as _GeographicMapSummary,
+from bijux_phylogenetics.phylogeography.geographic_map_review import (
+    GeographicMapArtifact,
+    GeographicMapExclusionRow,
+    GeographicMapLineRow,
+    GeographicMapMarkerRow,
+    GeographicMapReport,
+    GeographicMapSummary,
+    render_geographic_map_html,
+    summarize_continuous_phylogeography_map,
+    summarize_discrete_region_map,
+    write_geographic_map_exclusion_table,
+    write_geographic_map_line_table,
+    write_geographic_map_marker_table,
+    write_geographic_map_summary_table,
 )
 
-GeographicMapArtifact = _GeographicMapArtifact
-GeographicMapReport = _GeographicMapReport
-render_geographic_map_html = _render_geographic_map_html
-summarize_continuous_phylogeography_map = _summarize_continuous_phylogeography_map
-summarize_discrete_region_map = _summarize_discrete_region_map
-GeographicMapExclusionRow = _GeographicMapExclusionRow
-GeographicMapLineRow = _GeographicMapLineRow
-GeographicMapMarkerRow = _GeographicMapMarkerRow
-GeographicMapSummary = _GeographicMapSummary
-write_geographic_map_summary_table = _write_geographic_map_summary_table
-write_geographic_map_marker_table = _write_geographic_map_marker_table
-write_geographic_map_line_table = _write_geographic_map_line_table
-write_geographic_map_exclusion_table = _write_geographic_map_exclusion_table
+__all__ = [
+    "GeographicMapArtifact",
+    "GeographicMapExclusionRow",
+    "GeographicMapLineRow",
+    "GeographicMapMarkerRow",
+    "GeographicMapReport",
+    "GeographicMapSummary",
+    "render_geographic_map_html",
+    "summarize_continuous_phylogeography_map",
+    "summarize_discrete_region_map",
+    "write_geographic_map_exclusion_table",
+    "write_geographic_map_line_table",
+    "write_geographic_map_marker_table",
+    "write_geographic_map_summary_table",
+]
--- packages/bijux-phylogenetics/src/bijux_phylogenetics/phylogeography/geographic_map_review/__init__.py
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/phylogeography/geographic_map_review/__init__.py
@@ -1,5 +1,12 @@
 from __future__ import annotations
 
+from .artifact_outputs import (
+    write_geographic_map_exclusion_table,
+    write_geographic_map_line_table,
+    write_geographic_map_marker_table,
+    write_geographic_map_summary_table,
+)
+from .continuous_review import summarize_continuous_phylogeography_map
 from .contracts import (
     GeographicMapArtifact,
     GeographicMapExclusionRow,
@@ -8,6 +15,8 @@ from .contracts import (
     GeographicMapReport,
     GeographicMapSummary,
 )
+from .discrete_review import summarize_discrete_region_map
+from .presentation import render_geographic_map_html
 
 __all__ = [
     "GeographicMapArtifact",
@@ -16,4 +25,11 @@ __all__ = [
     "GeographicMapMarkerRow",
     "GeographicMapReport",
     "GeographicMapSummary",
+    "render_geographic_map_html",
+    "summarize_continuous_phylogeography_map",
+    "summarize_discrete_region_map",
+    "write_geographic_map_exclusion_table",
+    "write_geographic_map_line_table",
+    "write_geographic_map_marker_table",
+    "write_geographic_map_summary_table",
 ]
