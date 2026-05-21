ab5f013e308ba78cf7210f10789708ddff9fcf21
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-21 11:05:40 +0200

    refactor(render): establish tree figure package contracts

---
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/render/tree_figure_package/__init__.py
@@ -0,0 +1,27 @@
+"""Owned publication tree-figure package surface."""
+
+from __future__ import annotations
+
+from .contracts import (
+    FigureAnnotationCoverage,
+    FigureCaptionDraft,
+    FigureCollapsedCladeSummary,
+    FigureLegendAudit,
+    FigureLegendEntry,
+    FigureLegibilityAudit,
+    FigureTableConsistencyReport,
+    TreeFigureAuditReport,
+    TreeFigurePackageResult,
+)
+
+__all__ = [
+    "FigureAnnotationCoverage",
+    "FigureCaptionDraft",
+    "FigureCollapsedCladeSummary",
+    "FigureLegendAudit",
+    "FigureLegendEntry",
+    "FigureLegibilityAudit",
+    "FigureTableConsistencyReport",
+    "TreeFigureAuditReport",
+    "TreeFigurePackageResult",
+]
