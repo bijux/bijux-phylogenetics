1cf83dee93fd9578d9af0c589b7cbe6ac6a92318
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-29 01:22:04 +0200

    test(runtime): expose branch-length scaling proposal

--- packages/bijux-phylogenetics/tests/test_runtime.py
+++ packages/bijux-phylogenetics/tests/test_runtime.py
@@ -150,6 +150,7 @@ from bijux_phylogenetics.bayesian import (
     deserialize_bayesian_phylogenetic_state,
     deserialize_bayesian_phylogenetic_state_json,
     run_metropolis_hastings_sampler,
+    propose_branch_length_scaling_move,
     sample_prior_only_phylogenetic_state,
     evaluate_tree_topology_log_prior,
     score_bayesian_phylogenetic_state,
@@ -1223,6 +1224,10 @@ def test_public_package_exports_comparative_and_bayesian_workflows() -> None:
     assert (
         bayesian_api.build_metropolis_hastings_proposal
         is build_metropolis_hastings_proposal
+    )
+    assert (
+        bayesian_api.propose_branch_length_scaling_move
+        is propose_branch_length_scaling_move
     )
     assert (
         bayesian_api.score_bayesian_phylogenetic_state
