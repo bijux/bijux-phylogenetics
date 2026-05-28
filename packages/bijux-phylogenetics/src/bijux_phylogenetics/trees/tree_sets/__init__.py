7d235643cd12af10fcb28878f2276efd77a4a126
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-05-28 17:39:27 +0200

    feat(trees): compute quartet concordance factors

--- packages/bijux-phylogenetics/src/bijux_phylogenetics/trees/tree_sets/__init__.py
+++ packages/bijux-phylogenetics/src/bijux_phylogenetics/trees/tree_sets/__init__.py
@@ -5,31 +5,17 @@ from .budgets import (
     build_tree_set_workflow_budget,
     enforce_tree_set_tree_budget,
 )
-from .clade_support import (
-    compute_clade_frequency_table,
-    compute_reference_tree_clade_support,
-    write_clade_frequency_table,
-    write_reference_tree_clade_support_table,
-)
 from .clade_compatibility import (
     compute_clade_compatibility_graph,
     write_clade_compatibility_edge_table,
     write_clade_compatibility_graph_dot,
     write_clade_compatibility_node_table,
 )
-from .extended_consensus import (
-    compute_majority_rule_extended_consensus,
-    write_majority_rule_extended_consensus_artifacts,
-    write_majority_rule_extended_consensus_inclusion_table,
-    write_majority_rule_extended_consensus_rejected_conflict_table,
-)
-from .quartet_support import (
-    compute_reference_tree_quartet_support,
-    write_reference_tree_quartet_support_table,
-)
-from .quartet_puzzling import (
-    build_quartet_puzzling_consensus,
-    write_quartet_puzzling_artifacts,
+from .clade_support import (
+    compute_clade_frequency_table,
+    compute_reference_tree_clade_support,
+    write_clade_frequency_table,
+    write_reference_tree_clade_support_table,
 )
 from .consensus import (
     compute_consensus_tree,
@@ -38,32 +24,52 @@ from .consensus import (
     write_consensus_tree,
 )
 from .contracts import (
-    CladeFrequency,
-    CladeFrequencyReport,
     CladeCompatibilityEdgeRow,
     CladeCompatibilityGraphReport,
     CladeCompatibilityNodeRow,
+    CladeFrequency,
+    CladeFrequencyReport,
     ConsensusTreeReport,
+    GeneTreeQuartetConcordanceReport,
+    GeneTreeQuartetConcordanceRow,
     MajorityRuleExtendedAcceptedCladeRow,
     MajorityRuleExtendedConsensusReport,
     MajorityRuleExtendedRejectedCladeRow,
+    QuartetPuzzlingAssemblyRow,
+    QuartetPuzzlingReport,
+    QuartetTopologyScoreRow,
     TreeDistanceMatrixReport,
     TreeDistancePair,
     TreeSetCladeSupportReport,
     TreeSetCladeSupportRow,
-    QuartetPuzzlingAssemblyRow,
-    QuartetPuzzlingReport,
-    QuartetTopologyScoreRow,
+    TreeSetProcessingSummary,
     TreeSetQuartetSupportReport,
     TreeSetQuartetSupportRow,
-    TreeSetProcessingSummary,
     TreeSetRecord,
     TreeSetReport,
     TreeSetWorkflowBudget,
     TreeSetWorkflowBudgetReport,
 )
 from .distances import compute_tree_distance_matrix, write_tree_distance_matrix
+from .extended_consensus import (
+    compute_majority_rule_extended_consensus,
+    write_majority_rule_extended_consensus_artifacts,
+    write_majority_rule_extended_consensus_inclusion_table,
+    write_majority_rule_extended_consensus_rejected_conflict_table,
+)
 from .inventory import load_tree_set
+from .quartet_concordance import (
+    compute_gene_tree_quartet_concordance_factors,
+    write_gene_tree_quartet_concordance_table,
+)
+from .quartet_puzzling import (
+    build_quartet_puzzling_consensus,
+    write_quartet_puzzling_artifacts,
+)
+from .quartet_support import (
+    compute_reference_tree_quartet_support,
+    write_reference_tree_quartet_support_table,
+)
 
 __all__ = [
     "CladeFrequency",
@@ -72,6 +78,8 @@ __all__ = [
     "CladeCompatibilityGraphReport",
     "CladeCompatibilityNodeRow",
     "ConsensusTreeReport",
+    "GeneTreeQuartetConcordanceReport",
+    "GeneTreeQuartetConcordanceRow",
     "MajorityRuleExtendedAcceptedCladeRow",
     "MajorityRuleExtendedConsensusReport",
     "MajorityRuleExtendedRejectedCladeRow",
@@ -95,6 +103,7 @@ __all__ = [
     "compute_clade_compatibility_graph",
     "compute_clade_frequency_table",
     "compute_consensus_tree",
+    "compute_gene_tree_quartet_concordance_factors",
     "compute_majority_rule_extended_consensus",
     "compute_consensus_tree_with_threshold",
     "compute_reference_tree_clade_support",
@@ -108,6 +117,7 @@ __all__ = [
     "write_clade_compatibility_node_table",
     "write_clade_frequency_table",
     "write_consensus_tree",
+    "write_gene_tree_quartet_concordance_table",
     "write_majority_rule_extended_consensus_artifacts",
     "write_majority_rule_extended_consensus_inclusion_table",
     "write_majority_rule_extended_consensus_rejected_conflict_table",
